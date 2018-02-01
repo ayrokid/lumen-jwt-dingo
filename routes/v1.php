453426fb9d3ce3258035b8c502087a0c9f047481
ayrokid <ayrokid@gmail.com>
2018-02-01 09:57:13 +0700

    add router refresh token

--- routes/v1.php
+++ routes/v1.php
@@ -5,12 +5,10 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api\V1',
 ], function ($api) {
-
     $api->post('/register', 'AuthController@register');
-
     $api->post('/login', 'AuthController@login');
-
     $api->get('/me', 'AuthController@me');
+    $api->get('/refresh', 'AuthController@refresh');
 
     $api->post('/storage', 'StorageController@index');
 
